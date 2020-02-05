10c950510497cc4c97181ecb3b7d81cc237b5857
Amir Mursal <amir.mursal@communitybrands.com>
2020-02-05 19:01:11 +0530

    initial project commit

---
+++ MediNetCorp PDF Extractor/MediNetCorpPDF Extractor/MediNetCorpPDF Extractor/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MediNetCorpPDF_Extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_upload_excel = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_upload_excel
+            // 
+            this.btn_upload_excel.Location = new System.Drawing.Point(60, 109);
+            this.btn_upload_excel.Name = "btn_upload_excel";
+            this.btn_upload_excel.Size = new System.Drawing.Size(164, 23);
+            this.btn_upload_excel.TabIndex = 1;
+            this.btn_upload_excel.Text = "Upload Excel File";
+            this.btn_upload_excel.UseVisualStyleBackColor = true;
+            this.btn_upload_excel.Click += new System.EventHandler(this.btn_upload_excel_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btn_upload_excel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MediNetCorp Report Emailer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_upload_excel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ MediNetCorp PDF Extractor/MediNetCorpPDF Extractor/MediNetCorpPDF Extractor/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
+using System.Net.Mail;
+using System.Net;
+
+namespace MediNetCorpPDF_Extractor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DirectoryClean();
+        }
+
+        public void DirectoryClean()
+        {
+            try
+            {
+                string ExcelFiles = Path.Combine(Directory.GetCurrentDirectory() + @"\ExcelFiles");
+                string[] AllExcelFiles = Directory.GetFiles(ExcelFiles);
+                foreach (string file in AllExcelFiles)
+                {
+                    File.Delete(file);
+                }               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void btn_upload_excel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string excelFileDestination = Path.Combine(Directory.GetCurrentDirectory() + @"\ExcelFiles");
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Multiselect = true;
+                if (Directory.Exists(excelFileDestination))
+                {
+                    if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        foreach (string file in dlg.FileNames)
+                        {
+                            if (file.Contains(".xlsx") || file.Contains(".xls"))
+                            {
+                                File.Copy(file, Path.Combine(excelFileDestination, Path.GetFileName(file)), true);
+                                MessageBox.Show(file + " uploaded  Successfully");
+                                ProcessExcel(file);
+                                DirectoryClean();
+                            }
+
+                            else
+                            {
+                                MessageBox.Show("Upload Excel files only");
+                                return;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    System.IO.Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\ExcelFiles");
+                    if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        foreach (string file in dlg.FileNames)
+                        {
+                            if (file.Contains(".xlsx") || file.Contains(".xls"))
+                            {
+                                File.Copy(file, Path.Combine(excelFileDestination, Path.GetFileName(file)), true);
+                                MessageBox.Show(file + " uploaded  Successfully");
+                                ProcessExcel(file);
+                                DirectoryClean();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Upload Excel files only");
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        public void ProcessExcel(string file)
+        {
+
+            try
+            {  
+                //create the Application object we can use in the member functions.
+                Microsoft.Office.Interop.Excel.Application _excelApp = new Microsoft.Office.Interop.Excel.Application();
+                _excelApp.Visible = true;
+
+                //open the workbook
+                Workbook workbook = _excelApp.Workbooks.Open(file,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing);
+
+                //select the first sheet        
+                Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+
+                //find the used range in worksheet
+                Range excelRange = worksheet.UsedRange;
+
+                //get an object array of all of the cells in the worksheet (their values)
+                object[,] valueArray = (object[,])excelRange.get_Value(XlRangeValueDataType.xlRangeValueDefault);
+
+
+                var cellValue = (worksheet.Cells[2, 2] as Microsoft.Office.Interop.Excel.Range).Text;
+
+                int rowCount = worksheet.UsedRange.Rows.Count;
+                int columnCount = worksheet.UsedRange.Columns.Count;
+
+                List<string> columnValue = new List<string>();
+                List<string> BookmarkColumnValue = new List<string>();
+                Microsoft.Office.Interop.Excel.Range visibleCells = worksheet.UsedRange.SpecialCells(
+                                 Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeVisible,
+                           Type.Missing);
+                var dictionary = new List<KeyValuePair<string, string>>();
+                var providerDictionary = new List<KeyValuePair<string, string>>();
+                List<Tuple<string, string, string>> list = new List<Tuple<string, string, string>>();
+                foreach (Microsoft.Office.Interop.Excel.Range area in visibleCells.Areas)
+                {
+                    foreach (Microsoft.Office.Interop.Excel.Range row in area.Rows)
+                    {
+                        string softwareName = ((Microsoft.Office.Interop.Excel.Range)row.Cells[2, 2]).Text;
+                        if (softwareName != ""){
+                            
+                            SendEmail(softwareName);
+                        }
+                    }
+                }
+                MessageBox.Show("All Report has been send to perticular doctors");
+                workbook.Close(false, Type.Missing, Type.Missing);
+                _excelApp.Quit();
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void SendEmail(string softwareName)
+        {
+            try
+            {
+                var htmlBody = "<h1>"+ softwareName + "</h1>";
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("amirmursal@gmail.com");
+                message.To.Add(new MailAddress("amirthink72@gmail.com"));
+                message.Subject = "Test";
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = htmlBody;              
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("amirmursal@gmail.com", "amirarshin");
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }       
+
+    }
+
+}
